6d0bcc1fdfa9a39713030e833e8a1367c7cd5ea3
Steve <contact@stevedylan.dev>
2025-08-22 06:38:23 -0400

    chore: update email

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,44 +1,44 @@
 export const MENU_LINKS = [
-	{
-		title: "Home",
-		path: "/",
-	},
-	{
-		title: "About",
-		path: "/about",
-	},
-	{
-		title: "Blog",
-		path: "/posts",
-	},
-	{
-		title: "Projects",
-		path: "/projects",
-	},
-	{
-		title: "Videos",
-		path: "/videos",
-	},
-	{
-		title: "Photos",
-		path: "https://photos.stevedylan.dev",
-	},
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Blog",
+    path: "/posts",
+  },
+  {
+    title: "Projects",
+    path: "/projects",
+  },
+  {
+    title: "Videos",
+    path: "/videos",
+  },
+  {
+    title: "Photos",
+    path: "https://photos.stevedylan.dev",
+  },
 ];
 
 // ! Remember to add your own socials
 export const SOCIAL_LINKS = {
-	github: "https://github.com/stevedylandev",
-	twitter: "https://twitter.com/stevedylandev",
-	youtube: "https://youtube.com/@stevedylandev",
-	telegram: "https://telegram.me/stevedylandev",
-	farcaster: "https://farcaster.xyz/stevedylandev.eth",
-	medium: "https://medium.com/@stevedylandev",
-	linkedin: "https://www.linkedin.com/in/steve-simkins/",
-	photos: "https://photos.stevedylan.dev",
-	threads: "https://threads.net/stevedylandev",
-	website: "/",
-	coffee: "https://buymeacoffee.com/stevedylandev",
-	orbiter: "https://orbiter.host",
-	ethereum: "https://app.ens.domains/stevedylandev.eth",
-	email: "mailto:steve@terminal.ceo",
+  github: "https://github.com/stevedylandev",
+  twitter: "https://twitter.com/stevedylandev",
+  youtube: "https://youtube.com/@stevedylandev",
+  telegram: "https://telegram.me/stevedylandev",
+  farcaster: "https://farcaster.xyz/stevedylandev.eth",
+  medium: "https://medium.com/@stevedylandev",
+  linkedin: "https://www.linkedin.com/in/steve-simkins/",
+  photos: "https://photos.stevedylan.dev",
+  threads: "https://threads.net/stevedylandev",
+  website: "/",
+  coffee: "https://buymeacoffee.com/stevedylandev",
+  orbiter: "https://orbiter.host",
+  ethereum: "https://app.ens.domains/stevedylandev.eth",
+  email: "mailto:contact@stevedylan.dev",
 };
