550dc3ebd5b80606ee825d25e8b61fa3cf44f910
Steve <contact@stevedylan.dev>
2025-09-23 17:53:45 -0400

    chore: added post and updated rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -4,16 +4,18 @@ import siteMeta from "@/site-config";
 
 export async function GET() {
 	const posts = await getCollection("post");
+	const visiblePosts = posts.filter((post) => !post.data.hidden);
 
 	return rss({
 		title: siteMeta.title,
 		description: siteMeta.description,
 		site: "https://stevedylan.dev",
-		items: posts.map((post) => ({
+		items: visiblePosts.map((post) => ({
 			title: post.data.title,
 			description: post.data.description,
 			pubDate: post.data.publishDate,
 			link: `/posts/${post.slug}`,
+			content: post.body,
 		})),
 	});
 }
