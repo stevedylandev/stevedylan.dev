0c6187e27371d2160c86bf4ddb73bf08bf22f52f
Steve <contact@stevedylan.dev>
2026-03-16 08:34:19 -0400

    chore: updated paths

--- packages/client/src/env.d.ts
+++ packages/client/src/env.d.ts
@@ -1,7 +1,5 @@
 /// <reference path="../.astro/types.d.ts" />
 // <reference path="../.astro/types.d.ts" />
-/// <reference types="@astrojs/image/client" />
-/// <reference types="@clerk/astro/env" />
 
 // type Runtime = import("@astrojs/cloudflare").Runtime<ENV>;
 // declare namespace App {
