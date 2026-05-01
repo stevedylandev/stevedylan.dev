9d0a5825c9e57e709249cc1a107126db62ba1ff7
Steve <contact@stevedylan.dev>
2026-04-30 20:35:18 -0400

    chore: removed barrel import of shiki languages

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -1,33 +1,31 @@
 import MarkdownIt from "markdown-it";
 import { createHighlighterCore, type ThemeRegistrationRaw } from "shiki/core";
 import { createJavaScriptRegexEngine } from "shiki/engine/javascript";
-import { bundledLanguages } from "shiki";
 import darkmatter from "../../darkmatter.json";
 
 const LANGS = [
-	"javascript",
-	"typescript",
-	"jsx",
-	"tsx",
-	"python",
-	"bash",
-	"shell",
-	"json",
-	"html",
-	"css",
-	"rust",
-	"go",
-	"markdown",
-	"yaml",
-	"toml",
-	"text",
-	"lua",
-] as const;
+	import("@shikijs/langs/javascript"),
+	import("@shikijs/langs/typescript"),
+	import("@shikijs/langs/jsx"),
+	import("@shikijs/langs/tsx"),
+	import("@shikijs/langs/python"),
+	import("@shikijs/langs/bash"),
+	import("@shikijs/langs/shellscript"),
+	import("@shikijs/langs/json"),
+	import("@shikijs/langs/html"),
+	import("@shikijs/langs/css"),
+	import("@shikijs/langs/rust"),
+	import("@shikijs/langs/go"),
+	import("@shikijs/langs/markdown"),
+	import("@shikijs/langs/yaml"),
+	import("@shikijs/langs/toml"),
+	import("@shikijs/langs/lua"),
+];
 
 export async function createMarkdownRenderer(): Promise<MarkdownIt> {
 	const highlighter = await createHighlighterCore({
 		themes: [darkmatter as unknown as ThemeRegistrationRaw],
-		langs: LANGS.map((l) => bundledLanguages[l]),
+		langs: LANGS,
 		engine: createJavaScriptRegexEngine(),
 	});
 
