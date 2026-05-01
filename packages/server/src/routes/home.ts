434cb86dc6964dade901ea66c0778b36a206a09a
Steve <contact@stevedylan.dev>
2026-04-30 20:15:46 -0400

    chore: removed mono repo

--- packages/server/src/routes/home.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from "hono";
-
-const home = new Hono();
-
-home.get("/", (c) => {
-	return c.text("Hello Hono!");
-});
-
-export default home;
--- packages/server/src/routes/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as home } from "./home";
-export { default as now } from "./now";
-export { default as auth } from "./auth";
-// COMMENTS FUNCTIONALITY DISABLED
-// export { default as guestAuth } from "./guest-auth";
